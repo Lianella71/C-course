84dec6e6f5b4e39e4807c0251831f5c769b5ff8f
Olesia <ramisaly7@gmail.com>
2023-11-25 22:45:53 +0300

    Решили задачу 1

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// comment "Если нужно сделать комментарий на строку" комментарии не считываются кодом
+
+/*
+comment Если нужно сделать комментарий на несколько строк.
+comment Слэш использовать рядом с кнопкой Shift!
+*/
+/* // Consile.Write() - выводит в терминал данные, Console.WriteLine - данные с переходом на новую строку
+
+Console.Write("Введите число: ");
+
+// int - оператор целого числа, ReadLine () - считывает строку из консоли, Convert.ToInt32/64 и т.д конвертирует в число
+
+int f = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(f + 10);
+*/
+
+// ЗАДАЧА 1. 
+// Напишите программу которая на вход принимает два целых числа и проверяет является ли первое число квадратом второго
+// a = 25, b = 5 => yes
+// a = 2, b = 10 => no
+// a = 9, b = -3 => yes
+// a = -3, b = 9 => no
+/*
+Console.Clear(); //- очищает терминал при запуске программы
+Console.Write("Введите число: "); //- запрашивает в терминале ввод числа
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b)
+    Console.WriteLine("yes");
+else
+     Console.WriteLine("no")  ; 
+*/
