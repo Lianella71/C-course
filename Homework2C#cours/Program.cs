92bf93a507dcb2869d7af67da5dfef9e059fec9a
Olesia <ramisaly7@gmail.com>
2023-11-30 20:42:20 +0300

    Удаление файла

--- Homework2C#cours/Program.cs
+++ Homework2C#cours/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №1
+// Напишите программу, которая принимает на вход число
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+
