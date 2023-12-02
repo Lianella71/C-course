2e4e7fc0b3ab1fba26a67908d0c91b1014c9dc91
Olesia <ramisaly7@gmail.com>
2023-12-02 17:46:09 +0300

    Работа с задачей

--- Homework3C#course/Program.cs
+++ Homework3C#course/Program.cs
@@ -4,41 +4,25 @@
 // значения которых лежат в отрезке [20,90].
 /*
 Console.Clear();
-int n = 10;
-int[] array = new int[n];
-for (int i = 0; i < n; i++)
+
+int[] array = new int[10];
+for (int i = 0; i < array; i++)
 {
    array[i] = new Random().Next(1, 101);  // [1,100]
 Console.WriteLine(string.Join(", ", array));
 }
 
-int a = int.Parse (20);
-int b = int.Parse (90);
+int a = 20;
+int b = 90;
 int count = 0;
-for (int i =0; i < n; i++)
+for (int i =0; i < array; i++)
     if ((array[i] >= a) && (array[i] <= b)) count++;
 Console.WriteLine("Количество элементов в диапазоне " + count);
 Console.ReadKey();
 */
 
-int n = int.Parse(Console.ReadLine()); // вводим размер массива
-            int[] mas = new int[n]; //создаем массив
+
             
-            Random r = new Random();
-            for (int i = 0; i < n; i++)
-            {
-                mas[i] = r.Next(1, 100);  //заполняем массив числами от 1 до 100
-                Console.Write("{0,4}", mas[i]); // выводим массив на экран для контроля
-            }
-            Console.WriteLine();
- 
-            int A = int.Parse(Console.ReadLine());  // вводим с клавиатуры  A
-            int B = int.Parse(Console.ReadLine()); // вводим с клавиатуры В
-            int count = 0; //создаем счетчик
- 
-            for (int i = 0; i < n; i++) 
-                if ((mas[i] >= A) && (mas[i] <= B)) count++;  // считаем кол-во элементов mas[i] в диапазоне
-            Console.WriteLine("Количество элементов в диапазоне " + count); //выводим на экран
 
 
 
