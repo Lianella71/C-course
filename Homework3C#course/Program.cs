530c3b029f1a9eb0ae321dad4e0c705f08170636
Olesia <ramisaly7@gmail.com>
2023-12-02 18:13:41 +0300

    Решена первая задача ДЗ 3

--- Homework3C#course/Program.cs
+++ Homework3C#course/Program.cs
@@ -2,28 +2,25 @@
 // Задайте одномерный массив из 10 целых
 // чисел от 1 до 100. Найдите количество элементов массива,
 // значения которых лежат в отрезке [20,90].
-/*
+
 Console.Clear();
 
-int[] array = new int[10];
-for (int i = 0; i < array; i++)
-{
-   array[i] = new Random().Next(1, 101);  // [1,100]
-Console.WriteLine(string.Join(", ", array));
-}
-
-int a = 20;
-int b = 90;
-int count = 0;
-for (int i =0; i < array; i++)
-    if ((array[i] >= a) && (array[i] <= b)) count++;
-Console.WriteLine("Количество элементов в диапазоне " + count);
-Console.ReadKey();
-*/
-
-
+int n = 10; // вводим размер массива
+int[] mas = new int[n]; //создаем массив
             
-
-
-
+Random r = new Random();
+for (int i = 0; i < n; i++)
+    {
+        mas[i] = r.Next(1, 100);  //заполняем массив числами от 1 до 100
+        Console.Write($"{mas[i]} "); // выводим массив на экран для контроля
+    }
+Console.WriteLine();
+ 
+int A = 20;  // вводим с клавиатуры  A
+int B = 90; // вводим с клавиатуры В
+int count = 0; //создаем счетчик
+ 
+for (int i = 0; i < n; i++) 
+    if ((mas[i] >= A) && (mas[i] <= B)) count++;  // считаем кол-во элементов mas[i] в диапазоне
+    Console.WriteLine("Количество элементов лежащих в отрезке [20,90] - " + count); //выводим на экран
     
