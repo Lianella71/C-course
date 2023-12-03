f20f1170e9beb232f55c6d0909a23c46085edc51
Olesia <ramisaly7@gmail.com>
2023-12-03 13:15:26 +0300

    Решены все три задачи ДЗ 4 самопроверка

--- Homework4C#course/Program.cs
+++ Homework4C#course/Program.cs
@@ -4,27 +4,101 @@
 целые числа с консоли. Программа завершается при вводе
 символа ‘q’ или при вводе числа, сумма цифр которого чётная.
 */
-
-
-Console.Write("Введите число или 'q' для выхода: ");
-string input = Console.ReadLine(); // Чтение строки ввода пользователя
-if (input == "q") break; // Проверка на ввод 'q' для выхода
-int number;
-if (int.TryParse(input, out number)) // Проверка, является ли ввод числом{
-int sum = 0;
-while (number > 0) // Вычисление суммы цифр числа
+/*
+Console.Clear();
+while (true) 
 {
-    sum += number % 10; // Добавление последней цифры к сумме
-    number /= 10; // Удаление последней цифры из числа
+    Console.Write("Введите число или (q) для выхода: ");
+    string input = Console.ReadLine(); 
+    if (input == "q") 
+    {
+        Console.WriteLine("[Завершение программы]");
+        break; 
+    }
+    int number;
+    if (int.TryParse(input, out number))
+    {
+        int sum = 0;
+    while (number > 0) // Вычисление суммы цифр числа
+    {
+        sum += number % 10; // Добавление последней цифры к сумме
+        number /= 10; // Удаление последней цифры из числа
+    }
+    if (sum % 2 == 0) 
+    {
+        Console.WriteLine("[Остановка программы]");
+        break;
+    }
 }
-if (sum % 2 == 0) // Проверка, является ли сумма цифр четной
+else 
 {
-    Console.WriteLine("[STOP]");
-    break;
+Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
 }
-else  Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое числоили 'q'.");
+}
+*/
 
+//------------------------------------------------------------------------
 
+// == ЗАДАЧА № 2 ==
 
+/*
+Задайте массив заполненный случайными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел
+в массиве.
+*/
+/*
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randA = new int[a];
+Random random = new Random();
+int count = 0;
+for (int i = 0; i < randA.Length; i++)
+{
+    randA[i] = random.Next(100,1000);
+    Console.Write(randA[i] + " , ");
+    if(randA[i]%2==0)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел в массиве: {count}");
+*/
 
+//-------------------------------------------------------------------------------------------
+
+// == ЗАДАЧА № 3 == 
+/*
+Напишите программу, которая перевернёт одномерный массив
+(первый элемент станет последним, второй – предпоследним и т.д.) 
+*/
+/*
+Console.Clear();
+Console.Write("Введите количество элементов массива: "); // Ввести размер с консоли
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randA = new int[a];  // Сздала массив 
+Random random = new Random(); // Массив будет из случайных чисел
+int temporary;   // Ввела переменную для временного хранения
+for (int i = 0; i < randA.Length; i++)
+{
+    randA[i] = random.Next(1,100); // создан массив из случайных чисел в диапазоне
+    Console.Write(randA[i] + "  ");
+   
+}
+for (int i = 0; i < randA.Length / 2; i++) // Переворачиваем массив
+{
+    temporary = randA[i]; // заменила переменую
+    randA[i] = randA[randA.Length - 1 - i];
+    randA[randA.Length - 1 - i] = temporary;
+}
+
+Console.Write("\nПеревернутый массив: ");
+Console.WriteLine();
+foreach (int x in randA)
+{
+    Console.Write(x + "  ");
+}
+
+*/
 
